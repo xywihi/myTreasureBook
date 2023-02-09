67175f8d272d565bd5e0695166ce6357e1645ddf
anliang <liang.an@ncsi.com.cn>
2023-02-09 18:32:24 +0800

    对call、apply、bind方法原理进行了结束

--- application_of_JS/Type/index.js
+++ application_of_JS/Type/index.js
@@ -23,6 +23,17 @@ console.log({} instanceof Object);    // true
 
 // 能够区分Array、Object和Function，适合用于判断自定义的类实例对象，不能判断Number、Boolean、String。
 
+//自己定义方法，实现instanceof
+const myIndtanceof = (left,right) => {
+    if(typeof left !== 'object' || left == null) return false;
+    let proto = Object.getPrototypeOf(left);
+    while (true){
+        if (proto === null) return false;
+        if(proto === right.prototype) return true;
+        proto = Object.getPrototypeOf(proto)
+    }
+}
+
 //3、Object.prototype.toString.call()
 var toString = Onject.prototype.toString;
 console.log(toString.call(1));    // [object Number]
@@ -35,3 +46,4 @@ console.log(toString.call(undefined));    // [object Undefined]
 console.log(toString.call(null));    // [object Null]
 
 // 此判断方法组严谨，只是写法繁琐。
+
